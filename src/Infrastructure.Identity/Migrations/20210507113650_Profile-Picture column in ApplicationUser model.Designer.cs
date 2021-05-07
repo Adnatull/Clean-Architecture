b76fc10a87ae1a62d191391fbd46367651e363d0
Adnatull Al Masum <adnaan.masum@yahoo.com>
2021-05-07 18:08:05 +0600

    ProfilePicture byte property in ApplicationUser model

--- src/Infrastructure.Identity/Migrations/20210507113650_Profile-Picture column in ApplicationUser model.Designer.cs
+++ src/Infrastructure.Identity/Migrations/20210507113650_Profile-Picture column in ApplicationUser model.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Identity.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20210507113650_Profile-Picture column in ApplicationUser model")]
+    partial class ProfilePicturecolumninApplicationUsermodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure.Identity/Migrations/20210507113650_Profile-Picture column in ApplicationUser model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Identity.Migrations
+{
+    public partial class ProfilePicturecolumninApplicationUsermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Users");
+        }
+    }
+}
